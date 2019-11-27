f18b007e9a811d895e586ca7294de569f076f9d4
taranek <t.taranek27@gmail.com>
2019-11-27 21:43:44 +0100

    changing deployment config

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const theme = createMuiTheme({});
 
 function App() {
+  console.log('Updated app');
   console.log('rates api:',process.env.REACT_APP_RATES_API);
   return (
     <React.Fragment>
