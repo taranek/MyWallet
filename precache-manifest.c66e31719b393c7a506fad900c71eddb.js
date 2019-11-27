b252278201be57315d83b9551401665c9e6cc413
taranek <t.taranek27@gmail.com>
2019-11-27 19:20:12 +0100

    Updates

---
+++ precache-manifest.c66e31719b393c7a506fad900c71eddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7156b97c79da81fb2de5285cab778d77",
+    "url": "/MyWallet/index.html"
+  },
+  {
+    "revision": "f4a0375a2a1c9d513a68",
+    "url": "/MyWallet/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "70cacea07a7d0b6eedd8",
+    "url": "/MyWallet/static/css/main.53d1ed8c.chunk.css"
+  },
+  {
+    "revision": "f4a0375a2a1c9d513a68",
+    "url": "/MyWallet/static/js/2.9236aa6e.chunk.js"
+  },
+  {
+    "revision": "70cacea07a7d0b6eedd8",
+    "url": "/MyWallet/static/js/main.5a41f3f5.chunk.js"
+  },
+  {
+    "revision": "7f157aaaa302c209e4ac",
+    "url": "/MyWallet/static/js/runtime-main.3923dce9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d8462ef329512fb4c2faacc177144dc.js"
+  "/MyWallet/precache-manifest.c66e31719b393c7a506fad900c71eddb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyWallet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
