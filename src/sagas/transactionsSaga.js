c6348a0750cdaecb7ff988e52587fc17baf8fe9d
taranek <t.taranek27@gmail.com>
2019-12-01 22:44:23 +0100

    deploy config

--- src/sagas/transactionsSaga.js
+++ src/sagas/transactionsSaga.js
@@ -45,6 +45,6 @@ export function* transactionsSaga() {
   }
 }
 function getApiUrl(){
-  if (process.env.NODE_ENV === 'development') return process.env.REACT_APP_MY_WALLET_API_DEV;
-  return process.env.REACT_APP_MY_WALLET_API_PROD;
+  if (process.env.NODE_ENV !== 'development') return process.env.REACT_APP_MY_WALLET_API_PROD;
+  return process.env.REACT_APP_MY_WALLET_API_DEV;
 }
